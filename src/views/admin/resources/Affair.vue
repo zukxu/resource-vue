c90e890750b66b40dbd54f421238e118313e6980
zukxu <zukxu520@163.com>
2021-01-22 15:43:50 +0800

    feat(**): 新增事务处理页面
    新增事务处理页面
    
    ##

---
+++ src/views/admin/resources/Affair.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="Affair">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Affair',
+  props: {},
+  components: {},
+  created() {
+  },
+  data() {
+    return {}
+  },
+  methods: {},
+}
+</script>
+
+<style lang="less" scoped>
+#Affair{
+
+}
+</style>
