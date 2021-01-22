3d0d17a996008ef1d50429f60a90d06bae8db7e9
zukxu <zukxu520@163.com>
2021-01-22 14:43:29 +0800

    feat(Type.vue): 添加分类页面
    添加分类页面
    ##

---
+++ src/views/admin/resources/Type.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="Type">
+你好
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Type',
+  props: {},
+  components: {},
+  created() {
+  },
+  data() {
+    return {}
+  },
+  methods: {},
+}
+</script>
+
+<style lang="less" scoped>
+#Type{
+
+}
+</style>
